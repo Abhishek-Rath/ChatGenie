987d4ee3a0a9277bf4e7acb9973959a91bcaa1ed
Abhishek-Rath <abhi.rath39@gmail.com>
2023-04-14 19:39:58 +0530

    Feat: SQL translate - translate natural language into SQL queries

--- src/AIOptions/index.jsx
+++ src/AIOptions/index.jsx
@@ -39,6 +39,20 @@ const options = [
             stop: ["\n"],
         }
     },
+    {
+        name: "SQL Translate",
+        id: "sql",
+        description: "Translate natural language to SQL queries",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["#", ";"],
+        }
+    },
 
 ]
 
